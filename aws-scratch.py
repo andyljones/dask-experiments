d1f235d125aa08139a296da6fb235ace04b28b35
Andrew Jones <andyjones.ed@gmail.com>
2016-11-12 19:55:57 +0000

    Gets multi-machine communication working. Hooray :)

--- aws-scratch.py
+++ aws-scratch.py
@@ -6,15 +6,58 @@ Created on Sat Nov 12 17:01:14 2016
 @author: andyjones
 """
 
+import distributed
+import yaml
 import boto3
+import subprocess
+from contextlib import contextmanager
+from time import sleep
 
+KEYPAIR = 'andyljones-key-pair-us-east-1'
+
+def generate_config(scheduler_ip=None):
+    config = yaml.load(open('cloud-config.yaml'))
+    if scheduler_ip is None:
+        config['runcmd'].append('nohup dask-scheduler --bokeh-port 80 --port 8785')
+        config['runcmd'].append('nohup dask-worker --worker-port 8784 127.0.0.1:8785')
+    else:
+        config['runcmd'].append('nohup dask-worker --worker-port 8784 {}:8785'.format(scheduler_ip))
+        
+    config_string = '#cloud-config\n' + yaml.dump(config)
+    return config_string
+
+def create_instance(ec2, config, count=1):
+    return ec2.create_instances(ImageId='ami-b73b63a0', 
+                                MinCount=count, 
+                                MaxCount=count, 
+                                KeyName=KEYPAIR,
+                                UserData=config,
+                                SecurityGroups=['andyljones-ssh'],
+                                InstanceType='t2.small')
+
+def create_tunnel(public_dns_name, local_port, remote_port):
+    destination = 'ec2-user@' + public_dns_name
+    tunnel = str(local_port) + ':' + public_dns_name + ':' + str(remote_port)
+    key_pair = KEYPAIR + '.pem'
+    tunnel = subprocess.Popen(['ssh', '-i', key_pair, '-N', '-L', tunnel, destination])
+    return tunnel
+        
 def run():
     ec2 = boto3.resource('ec2')
-    instances = ec2.create_instances(ImageId='ami-b73b63a0', 
-                                     MinCount=1, 
-                                     MaxCount=1, 
-                                     KeyName='andyljones-key-pair-us-east-1',
-                                     UserData=open('cloud-config.yaml').read(),
-                                     SecurityGroups=['andyljones-ssh'],
-                                     InstanceType='t2.small')
-    instance = instances[0]
+    
+    [scheduler] = create_instance(ec2, generate_config())
+    workers = create_instance(ec2, generate_config(scheduler.private_ip_address))
+    
+    scheduler_tunnel = create_tunnel(scheduler.public_dns_name, '8785', '8785')
+    http_tunnel = create_tunnel(scheduler.public_dns_name, '2000', '80')
+    
+    executor = distributed.Executor('127.0.0.1:8785')
+    future = executor.submit(lambda n: sum(range(1, n)), 10000)
+    print(future.result())
+    
+    scheduler_tunnel.terminate()
+    http_tunnel.terminate()
+    scheduler.terminate()
+    for w in workers:
+        w.terminate()
+    
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@
     * Create a user called 'ec2-admin' or similar. Make a note of the access key ID and secret key, which will be used to access the API.
     * Give the user the "AmazonEC2FullAccess" policy. You can attach the policies directly to the user; no need to create groups just yet. 
     * Create a key pair, say 'andyljones-key-pair-us-east-1'. Save it locally. This'll be used to connect to container instances via ssh
-    * Create a security group that allows inbound access through port 22, say 'andyljones-ssh'.
+    * Create a security group that allows inbound access through port 22, say 'andyljones-ssh'. Also create another rule that allows all inbound traffic from the same security group.
  * Install the AWS CLI using "pip install awscli"
  * Set up a default configuration using "aws configure". Pass it the ID and secret you got when creating the 'ec2-admin' user. Set the default region to whatever the default region for the key pair was (probably us-east).
  * Install boto3.
